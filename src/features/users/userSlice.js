8399330359e978ccef02795ef332330d812869ce
SanjayKumar-M <sanjaymoad786@gmail.com>
2022-07-03 20:00:34 +0530

    Second Commit

--- src/features/users/userSlice.js
+++ src/features/users/userSlice.js
@@ -1,38 +1,4 @@
-// import { createSlice } from "@reduxjs/toolkit";
 
-// var initialState = {
-//     name: "",
-//     email: "",
-//     photo: ""
-// }
-
-// var userSlice = createSlice(
-
-//     name : "users",
-//     initialState,
-    
-//     reducers: {
-//         setUserLogin: (state, action) => {
-//             state.name = action.payload.name,
-//             state.email = action.payload.email,
-//             state.photo = action.payload.photo
-//         },
-
-//         setSignOut: (state) =>{
-//             state.name = null;
-//             state.email = null;
-//             state.photo = null;
-//         }
-//     }
-// );
-
-// export const {setUserLogin, setSignOut} = userSlice.action;
-
-// export const selectUserName = (state) => state.user.name; 
-// export const selectUserEmail = (state) => state.user.email; 
-// export const selectUserPhoto = (state) => state.user.photo; 
-
-// export default userSlice.reducer;
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
